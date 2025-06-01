8cdc4b1bb4e6be66ae18ba67c0a2a70eb13030ea
dev <dev@info.com>
2025-06-01 11:02:32 +0330

    Add InstallmentEntityTypeConfiguration class
    Introduces the `InstallmentEntityTypeConfiguration` class
    to configure the `Installment` entity in Entity Framework Core.
    Defines table name, primary key, and property requirements
    including data types for `DueDate`, `AmountDue`, and
    `AmountPaid`.

---
+++ StoreManagement.Infrastructure/Data/Configurations/InstallmentEntityTypeConfiguration.cs
@@ -0,0 +1,32 @@
+﻿namespace StoreManagement.Infrastructure.Data.Configurations;
+
+public class InstallmentEntityTypeConfiguration : IEntityTypeConfiguration<Installment>
+{
+    public void Configure(EntityTypeBuilder<Installment> builder)
+    {
+        builder.ToTable("Installments");
+        builder.HasKey(i => i.Id);
+        builder.Property(i => i.Id)
+            .ValueGeneratedOnAdd()
+            .IsRequired();
+
+        builder.Property(i => i.InvoiceId)
+            .IsRequired();
+        builder.Property(i => i.InvoiceType)
+            .IsRequired();
+        builder.Property(i => i.InstallmentNumber)
+            .IsRequired();
+        builder.Property(i => i.DueDate)
+            .IsRequired()
+            .HasColumnType("datetime2");
+        builder.Property(i => i.AmountDue)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)");
+        builder.Property(i => i.AmountPaid)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)");
+        builder.Property(i => i.Status)
+            .IsRequired();
+
+    }
+}
